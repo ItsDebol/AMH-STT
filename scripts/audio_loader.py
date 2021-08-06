c5e4ba3f1b66f1d44d92d76b6e1b617c942d6b55
DiboraHaile <deborahaile3@gmail.com>
2021-08-06 17:05:20 +0300

    update load_audio function

--- scripts/audio_loader.py
+++ scripts/audio_loader.py
@@ -29,7 +29,7 @@ class AudioLoader(AudioExplorer):
         # but first we need to change the load
 
     def load_audio(self):
-
+        
         try:
             audio_name = []
             audio_mode = []
@@ -38,10 +38,9 @@ class AudioLoader(AudioExplorer):
             audio_duration = []
             has_TTS = []
             tts = []
-
-            for audio_file in self.audio_files:
+            
+            for audio_file in self.audio_files_dir_list:
                 audio_data, audio_freq = lb.load(audio_file,sr=None)
-                # Audio_Name
                 name = audio_file.split('wav')[-2]
                 name = name[1:-1].strip()
                 audio_name.append(name)
